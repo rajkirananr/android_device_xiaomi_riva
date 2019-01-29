f2af6fc47ff13a3fbf504f41c9c0c60c30c4c3fd
rajkirananr <47035914+rajkirananr@users.noreply.github.com>
2019-01-29 11:05:18 +0530

    Update bootleg_riva.mk

--- bootleg_riva.mk
+++ bootleg_riva.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Bootleggers stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk/)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
