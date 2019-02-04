3fd6b44d7021ecda5819b77b13ff6894ffffb73b
rajkirananr <47035914+rajkirananr@users.noreply.github.com>
2019-02-04 12:47:06 +0530

    Update and rename bootleg_riva.mk to colt_riva.mk

--- colt_riva.mk
+++ colt_riva.mk
@@ -20,8 +20,9 @@ DEVICE_PATH := device/xiaomi/riva
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Bootleggers stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+# Inherit some common Colt stuff.
+$(call inherit-product, vendor/colt/common.mk)
+
 
 # Inherit from riva device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,7 +31,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := bootleg_riva
+PRODUCT_NAME := colt_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
